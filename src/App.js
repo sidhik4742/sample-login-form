0a1ad07300450690a0952e0bb207544de09323e8
sidhik4742 <sidhik64742@gmail.com>
2020-12-09 10:38:57 +0530

    router testing

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  HashRouter,
+  Route,
+  Switch,
+} from 'react-router-dom';
 import './App.css';
 import Login from './Component/Login/Login';
 import Footer from './Component/Footer/Footer';
-
+import Home from './Component/Home/Home';
 
 function App() {
+  console.log('====================================');
+  console.log("App.js");
+  console.log('====================================');
   return (
     <div className="App">
-      <Login/>
-      <Footer/>
+      <Router>
+        <Route path="/" exact>
+          <Login />
+          <Footer />
+        </Route>
+        <Route path="/home" >
+          <Home/>
+        </Route>
+      </Router>
     </div>
   );
 }
--- src/Component/Login/Login.js
+++ src/Component/Login/Login.js
@@ -1,83 +1,87 @@
-import React, { Component } from "react";
-import "./Login.css";
+import React, {Component, useState} from 'react';
+import {Link, useHistory} from 'react-router-dom';
 
-class Login extends Component {
-  state = {
-    dp: "",
-  };
+import './Login.css';
 
-  uploadImage = (event) => {
-    console.log("Upload image");
+function Login() {
+  const history = useHistory();
+  const [dp, setDp] = useState();
+
+  const uploadImage = (event) => {
+    console.log('Upload image');
     let type = event.target.files[0].type;
-    if (type == "image/jpeg" || type == "image/jpg") {
+    if (type == 'image/jpeg' || type == 'image/jpg') {
       console.log(event.target.files[0]);
-      this.setState({
+      setDp({
         dp: URL.createObjectURL(event.target.files[0]),
       });
-      console.log(this.state.dp);
+      console.log(dp);
     }
   };
 
-  profilePicture = (event) => {
-    console.log("profile picture clicked");
+  const profilePicture = (event) => {
+    console.log('profile picture clicked');
+    // history.push('/home');
   };
 
-  loginButton = () => {
-    console.log("Button clicked");
+  const loginButton = (event) => {
+    // event.preventDefault();
+    console.log('Button clicked');
   };
 
-  render() {
-    return (
-      <div className="login">
-        <main>
-          <form className="form">
-            <div className="img-div">
-              <label htmlFor="uploadImage">
-                <img
-                  className="image"
-                  onClick={this.profilePicture}
-                  src={
-                    this.state.dp
-                      ? this.state.dp
-                      : "https://cdn2.f-cdn.com/contestentries/1316431/24595406/5ae8a3f2e4e98_thumb900.jpg"
-                  }
-                  alt=""
-                />
-              </label>
-              <input
-                type="file"
-                id="uploadImage"
-                onChange={this.uploadImage}
-                style={{ display: "none" }}
+  return (
+    <div className="login">
+      <main>
+        <form className="form">
+          <div className="img-div">
+            <label htmlFor="uploadImage">
+              <img
+                className="image"
+                onClick={profilePicture}
+                src={
+                  dp
+                    ? dp
+                    : 'https://cdn2.f-cdn.com/contestentries/1316431/24595406/5ae8a3f2e4e98_thumb900.jpg'
+                }
+                alt=""
               />
-            </div>
-            <div>
-              <span>
-                <i className="fa fa-user fa-2x" aria-hidden="true"></i>
-              </span>
-              <input type="text" placeholder="user name" />
-            </div>
-            <div>
-              <span>
-                <i className="fa fa-lock fa-2x" aria-hidden="true"></i>
-              </span>
-              <input type="password" placeholder="password" />
-            </div>
-            <div className="btn-div">
-              <button type="button" onClick={this.loginButton}>
+            </label>
+            <input
+              type="file"
+              id="uploadImage"
+              onChange={uploadImage}
+              style={{display: 'none'}}
+            />
+          </div>
+          <div>
+            <span>
+              <i className="fa fa-user fa-2x" aria-hidden="true"></i>
+            </span>
+            <input type="text" placeholder="user name" />
+          </div>
+          <div>
+            <span>
+              <i className="fa fa-lock fa-2x" aria-hidden="true"></i>
+            </span>
+            <input type="password" placeholder="password" />
+          </div>
+          <div className="btn-div">
+            <Link to="/home/content" >
+              {' '}
+              <button type="button" onClick={loginButton}>
                 Log In
               </button>
-            </div>
-          </form>
-          <div className="temsService">
-            <p>By Registering, | accept the</p>
-            <a href="#"> Terms of Service </a> <span>and </span>
-            <a href="#"> Privacy Policy</a>
+            </Link> 
           </div>
-        </main>
-      </div>
-    );
-  }
+        </form>
+        <div className="temsService">
+          <p>By Registering, | accept the</p>
+          <a href="#"> Terms of Service </a> <span>and </span>
+          <a href="#"> Privacy Policy</a>
+        </div>
+      </main>
+    </div>
+  );
 }
 
 export default Login;
